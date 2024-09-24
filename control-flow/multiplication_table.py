d9cb413714801501cf185c2f97d959f9c74eb266
Feven237 <fevenwolde54@gmail.com>
2024-09-24 06:01:31 +0400

    Update multiplication_table.py
    i change

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
-
-# Get user input
 number = int(input("Enter a number to generate its multiplication table: "))
 
-# Generate the multiplication table
 for i in range(1, 11):  # Correct range for 1 to 10
     result = number * i
     print(f"{number} x {i} = {result}")
